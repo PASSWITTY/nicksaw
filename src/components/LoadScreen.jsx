0b91652b4a8fe5c59e6722b583a733623bfd9d60
Admin <dkipkerui@gmail.com>
2024-04-18 12:50:32 +0300

    third

--- src/components/LoadScreen.jsx
+++ src/components/LoadScreen.jsx
@@ -3,7 +3,7 @@
 const LoadingScreen = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white rounded-full p-4 flex items-center space-x-4">
+      <div className="bg-zinc-300 rounded-full p-4 flex items-center space-x-4">
         <div className="w-4 h-4 rounded-full bg-indigo-500 animate-pulse"></div>
         <div className="w-4 h-4 rounded-full bg-indigo-500 animate-pulse delay-75"></div>
         <div className="w-4 h-4 rounded-full bg-indigo-500 animate-pulse delay-150"></div>
